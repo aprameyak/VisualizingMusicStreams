d37002cbc32b5f09b19fd11d07cf84ad57d04e67
Aprameya Kannan <101409453+aprameyak@users.noreply.github.com>
2025-05-20 00:57:58 -0400

    Update musicstreams.py

--- musicstreams.py
+++ musicstreams.py
@@ -1,39 +1,36 @@
-import os
 import pandas as pd
-import scipy as sp
-import scipy.stats  as stats
+import scipy.stats as stats
 import matplotlib.pyplot as plt
 
-df = pd.read_csv(r'MostStreamed.csv')
-data = df[['Track Score','Apple Music Playlist Count', 'Amazon Playlist Count', 'Deezer Playlist Count']]
-df = pd.DataFrame(data, columns=['Track Score','Apple Music Playlist Count', 'Amazon Playlist Count', 'Deezer Playlist Count'])
+df = pd.read_csv('MostStreamed.csv')
+df = df[['Track Score', 'Apple Music Playlist Count', 'Amazon Playlist Count', 'Deezer Playlist Count']]
 
-df['Apple Music Playlist Count'] = df['Apple Music Playlist Count'].fillna(0)
-df['Amazon Playlist Count'] = df['Amazon Playlist Count'].fillna(0)
-df['Deezer Playlist Count'] = df['Deezer Playlist Count'].fillna(0)
+df.fillna({'Apple Music Playlist Count': 0,
+           'Amazon Playlist Count': 0,
+           'Deezer Playlist Count': 0}, inplace=True)
 
-correlA = stats.pearsonr(df['Track Score'], df['Apple Music Playlist Count'])
-correlB = stats.pearsonr(df['Track Score'], df['Amazon Playlist Count'])
-correlC = stats.pearsonr(df['Track Score'], df['Deezer Playlist Count']
-                         )
-corA = "Apple Music Correlation: " + str(correlA)[25: 29]
-corB = "Amazon Correlation: " + str(correlB)[25: 29]
-corC = "Deezer Correlation: " + str(correlC)[25: 29]
+correl_apple = stats.pearsonr(df['Track Score'], df['Apple Music Playlist Count'])[0]
+correl_amazon = stats.pearsonr(df['Track Score'], df['Amazon Playlist Count'])[0]
+correl_deezer = stats.pearsonr(df['Track Score'], df['Deezer Playlist Count'])[0]
 
-font1 = {'family':'serif','color':'purple','size':20}
-font2 = {'family':'serif','color':'darkred','size':15}
+corA = f"Apple Music Correlation: {correl_apple:.2f}"
+corB = f"Amazon Correlation: {correl_amazon:.2f}"
+corC = f"Deezer Correlation: {correl_deezer:.2f}"
 
-df.plot(x="Track Score", y=['Apple Music Playlist Count', 'Amazon Playlist Count', 'Deezer Playlist Count'],
+font1 = {'family': 'serif', 'color': 'purple', 'size': 20}
+font2 = {'family': 'serif', 'color': 'darkred', 'size': 15}
+
+df.plot(x="Track Score", 
+        y=['Apple Music Playlist Count', 'Amazon Playlist Count', 'Deezer Playlist Count'],
         kind="line", figsize=(10, 10))
 
-plt.title('Playlist Count Vs Track Score', fontdict=font1)
-
-plt.figtext(.67, .79, corA)
-plt.figtext(.67, .77, corB)
-plt.figtext(.67, .75, corC)
-
+plt.title('Playlist Count vs Track Score', fontdict=font1)
 plt.xlabel('Track Score', fontdict=font2)
 plt.ylabel('Playlist Count', fontdict=font2)
 
-plt.show()
+plt.figtext(0.67, 0.79, corA)
+plt.figtext(0.67, 0.77, corB)
+plt.figtext(0.67, 0.75, corC)
 
+plt.tight_layout()
+plt.show()
